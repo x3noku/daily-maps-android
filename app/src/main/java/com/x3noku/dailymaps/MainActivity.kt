ab607dd6c0545a76b112ca02c9fbea0862354e25
x3noku <Artem.Kaikov@yandex.ru>
2019-11-19 01:08:38 +0600

    Initial commit

---
+++ app/src/main/java/com/x3noku/dailymaps/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.x3noku.dailymaps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
